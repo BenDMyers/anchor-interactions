0fae089355482f07b7e2cba1d13c84ac75ae58f5
Ben Myers <ben.myers@okstate.edu>
2023-09-29 15:04:36 -0500

    Add a lil description

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const sass = require('eleventy-sass');
 const kbd = require('markdown-it-kbd');
+const moment = require('moment');
 
 /**
  * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
@@ -9,9 +10,12 @@ const kbd = require('markdown-it-kbd');
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(sass);
 	eleventyConfig.amendLibrary('md', md => md.use(kbd));
+	eleventyConfig.addFilter('date', (date, format) => moment(date).utc().format(format));
 
 	const encodedUri = encodeURIComponent('https://anchor-interactions.netlify.app/')
 	eleventyConfig.addGlobalData('openGraphImage', `https://v1.screenshot.11ty.dev/${encodedUri}/opengraph/`);
+
+	eleventyConfig.addGlobalData('buildDate', new Date());
 	
 	return {
 		dir: {
